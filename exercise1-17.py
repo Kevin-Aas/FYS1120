04b1cb94a68788632a18920d74183d390d570c26
Kevin Aslesen <kevinalexander@live.no>
2022-09-02 17:03:56 +0200

    yeah

---
+++ exercise1-17.py
@@ -0,0 +1,20 @@
+import sympy as sy
+r = sy.Symbol('r')
+a = sy.Symbol('a')
+z = sy.Symbol('z')
+f = r/(r**2 + z**2)**(3/2)
+print(sy.integrate(f, (r,0,a)))
+
+import numpy as np
+import matplotlib.pyplot as plt
+rho_A = 1
+epsilon0 = 8.854187817e-12
+a = 1
+z = np.linspace(0, 20, 100)
+
+E = rho_A*z/(2*epsilon0) * (1/z - 1/(z**2 + a**2)**(1/2))
+
+plt.plot(z, E)
+plt.xlabel("z")
+plt.ylabel("E", rotation=0)
+plt.show()
